35f0bb7c65064c2fcd1009a004bd9573037555a7
takuto1127 <tiharu717@gmail.com>
2025-09-30 10:27:16 +0900

    update build script to use wasi-sdk-21 for improved compatibility

--- build.bash
+++ build.bash
@@ -6,11 +6,9 @@ mkdir build
 
 cd build
 
-# https://github.com/bytecodealliance/wasm-micro-runtime/blob/WAMR-1.1.1/doc/pthread_library.md
-/opt/wasi-sdk-19/bin/llvm-ar -d /opt/wasi-sdk-19/share/wasi-sysroot/lib/wasm32-wasi/libc.a dlmalloc.o
-cp ${WAMR_ROOT}/wamr-sdk/app/libc-builtin-sysroot/include/pthread.h /opt/wasi-sdk-19/share/wasi-sysroot/include
+# Build using wasi-libc threads (no WAMR sysroot hacks)
 
-cmake -DWASI_SDK_PREFIX=/opt/wasi-sdk-19 -DCMAKE_TOOLCHAIN_FILE=/opt/wasi-sdk-19/share/cmake/wasi-sdk.cmake -DCMAKE_SYSROOT=/opt/wasi-sdk-19/share/wasi-sysroot .. 
+cmake -DWASI_SDK_PREFIX=/opt/wasi-sdk-21 -DCMAKE_TOOLCHAIN_FILE=/opt/wasi-sdk-21/share/cmake/wasi-sdk.cmake -DCMAKE_SYSROOT=/opt/wasi-sdk-21/share/wasi-sysroot .. 
 
 make
 
